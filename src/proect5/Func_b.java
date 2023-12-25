2197eb8bc6ece14809462fd396665c1f979b30a9
KostoPodjanin <kostopodjanin@gmail.com>
2023-12-25 23:41:03 +0100

    proect5

---
+++ src/proect5/Func_b.java
@@ -0,0 +1,15 @@
+package proect5;
+
+public class Func_b {
+
+    public static void main(String[] args) {
+        int a = 10;
+        myFunc(a);
+    }
+    public static void myFunc(int a) {
+        int b;
+        b = a*3;
+        System.out.println(b);
+    }
+
+}
